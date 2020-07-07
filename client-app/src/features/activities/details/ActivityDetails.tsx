88cc7d148dd969470a2aa6457d9458387ad92abe
unknown <SVSCHNLT@SVSCHNLT267.SVSGLOBAL.COM>
2020-07-07 20:52:43 +0530

    End of section 8

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,12 +1,34 @@
-import React, { useContext } from "react";
-import { Card, Image,  Button } from "semantic-ui-react";
-import ActivityStore from '../../../app/stores/activityStore';
+import React, { useContext, useEffect } from "react";
+import { Card, Image, Button } from "semantic-ui-react";
+import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router";
+import { LoadingComponent } from "../../../app/layout/LoadingComponent";
+import { Link } from "react-router-dom";
 
- const ActivityDetails: React.FC = () => {
- const activityStore = useContext(ActivityStore);
-  const {selectedActivity: activity, openEditForm, cancelSelectedActivity} = activityStore;
- return (
+interface DetailParms {
+  id: string;
+}
+
+const ActivityDetails: React.FC<RouteComponentProps<DetailParms>> = ({
+  match,
+  history
+}) => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    activity,
+    loadActivity,
+    loadingInitial
+  } = activityStore;
+
+  useEffect(() => {
+    loadActivity(match.params.id);
+  }, [loadActivity, match.params.id]);
+
+  if (loadingInitial || !activity)
+  return <LoadingComponent content = 'Loading activity.....'/>;
+
+  return (
     <Card fluid>
       <Image
         src={`/assets/categoryImages/${activity!.category}.jpg`}
@@ -23,13 +45,13 @@ import { observer } from "mobx-react-lite";
       <Card.Content extra>
         <Button.Group widths={2}>
           <Button
-            onClick={() => openEditForm(activity!.id)}
+            as={Link} to={`/manage/${activity.id}`} 
             basic
             color="blue"
             content="Edit"
           />
           <Button
-            onClick={cancelSelectedActivity }
+            onClick={() => {history.push('/activities')}}
             basic
             color="grey"
             content="Cancel"
@@ -40,4 +62,4 @@ import { observer } from "mobx-react-lite";
   );
 };
 
-export default observer(ActivityDetails)
+export default observer(ActivityDetails);
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,22 +1,32 @@
-import React, { useContext } from "react";
-import { Menu, Button } from "semantic-ui-react";
-import ActivityStore from '../../app/stores/activityStore';
+import React from "react";
+import { Menu, Button, Container } from "semantic-ui-react";
+
 import { observer } from "mobx-react-lite";
+import {  NavLink } from "react-router-dom";
 
-
- const NavBar: React.FC = () => {
-  const activityStore = useContext(ActivityStore);
+const NavBar: React.FC = () => {
+  
   return (
-    <Menu fixed='top' inverted>
-      <Menu.Item>
-        <img src="/assets/logo.png" alt = "logo" style={{marginRight: '10px'}}/>
-        Reactivities
-      </Menu.Item>
-      <Menu.Item name="Activities" />
-      <Menu.Item >
-        <Button onClick={activityStore.openCreateForm} positive content="Create Activity"/>
-      </Menu.Item>
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header as = {NavLink} exact to = '/'>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" as={NavLink} to='/activities'/>
+        <Menu.Item>
+          <Button
+            as={NavLink} to='/CreateActivity'
+            positive
+            content="Create Activity"
+          />
+        </Menu.Item>
+      </Container>
     </Menu>
   );
 };
-export default observer( NavBar);
+export default observer(NavBar);
