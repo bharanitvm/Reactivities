9ddd74d5659593a720bea7b2014c25e697f1142f
Bharani <bharanitvm@gmail.com>
2021-03-04 17:34:48 +0530

    reg format

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -20,8 +20,11 @@ namespace Application.User
         public class Command : IRequest<User>
         {
             public string DisplayName { get; set; }
+
             public string UserName { get; set; }
+
             public string Email { get; set; }
+
             public string Password { get; set; }
         }
 
@@ -39,48 +42,61 @@ namespace Application.User
         public class Handler : IRequestHandler<Command, User>
         {
             private readonly DataContext _context;
+
             private readonly UserManager<AppUser> _userManager;
+
             private readonly IJwtGenerator _jwtGenerator;
 
-            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            public Handler(
+                DataContext context,
+                UserManager<AppUser> userManager,
+                IJwtGenerator jwtGenerator
+            )
             {
                 _context = context;
                 _userManager = userManager;
                 _jwtGenerator = jwtGenerator;
             }
 
-            public async Task<User> Handle(Command request,
-                    CancellationToken cancellationToken)
+            public async Task<User>
+            Handle(Command request, CancellationToken cancellationToken)
             {
-                if(await _context.Users.AnyAsync(x => x.Email == request.Email))
-                    throw new RestException(HttpStatusCode.BadRequest, new {Email = "Email already exists"} );
+                if (await _context.Users.AnyAsync(x => x.Email == request.Email)
+                )
+                    throw new RestException(HttpStatusCode.BadRequest,
+                        new { Email = "Email already exists" });
 
-                if(await _context.Users.AnyAsync(x => x.UserName == request.UserName))
-                    throw new RestException(HttpStatusCode.BadRequest, new {UserName = "UserName already exists"} );
+                if (
+                    await _context
+                        .Users
+                        .AnyAsync(x => x.UserName == request.UserName)
+                )
+                    throw new RestException(HttpStatusCode.BadRequest,
+                        new { UserName = "UserName already exists" });
 
-                var user = new AppUser
+                var user =
+                    new AppUser {
+                        DisplayName = request.DisplayName,
+                        Email = request.Email,
+                        UserName = request.UserName
+                        //Password = request.Password
+                    };
+
+                var result =
+                    await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
                 {
-                    DisplayName = request.DisplayName,
-                    Email = request.Email,
-                    UserName = request.UserName
-                    //Password = request.Password
-                };
-
-                var result = await _userManager.CreateAsync(user,request.Password);
-
-                if (result.Succeeded) {
-                    return new User
-                    {
+                    return new User {
                         DisplayName = request.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
-                        UserName= request.UserName,
+                        UserName = request.UserName,
                         Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                     };
-                };
+                }
 
                 throw new Exception("Problem creating user");
             }
         }
-
     }
 }
